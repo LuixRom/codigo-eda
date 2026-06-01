f8ae4bdd26bc8263d476e936c317efe64f8813cf
AnthRom <luis.romero@utec.edu.pe>
2026-06-01 01:43:49 -0500

    Subiendo el Sparse Table

--- punto-extra.cpp
+++ punto-extra.cpp
@@ -1,7 +1,35 @@
 #include<iostream>
 #include<vector>
+#include <algorithm>
+#include <map>
 
 using namespace std;
+
+struct SparseTable {
+    vector<vector<int>> sparse_table;
+
+    SparseTable() {}
+
+    SparseTable(vector<int>& a) {
+        int n = a.size();
+        sparse_table.clear();
+        sparse_table.emplace_back(a);
+        for (int k = 1; (1 << k) <= n; k++) {
+            sparse_table.emplace_back(n - (1 << (k - 1)) + 1);
+            sparse_table[k].resize(n - (1 << k) + 1);
+            for (int i = 0; i + (1 << k) - 1 < n; i++) {
+                sparse_table[k][i] = min(sparse_table[k-1][i],
+                                         sparse_table[k-1][i + (1 << (k-1))]);
+            }
+        }
+    }
+
+    int query(int l, int r) {
+        int length = r - l + 1;
+        int d = 31 - __builtin_clz(length);
+        return min(sparse_table[d][l], sparse_table[d][r - (1 << d) + 1]);
+    }
+}
 
 int main(){
     int N, R;
